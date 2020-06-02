e00e83ce2e430136da3729395cb58b002ac905fb
Markus Mengel <markus.mengelhx@gmail.com>
2020-06-02 14:26:11 +0200

    created flat component

---
+++ src/components/flat.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Flat extends Component {
+  render() {
+    const {
+      name,
+      imageUrl,
+      price,
+      priceCurrency
+    } = this.props;
+    if (!name) {
+      return null;
+    }
+    return (
+      <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${imageUrl}')` }}>
+        <div className="card-category">{price} {priceCurrency}</div>
+        <div className="card-description">
+          <h2>{name}</h2>
+        </div>
+        <a className="card-link" href="#"></a>
+      </div>
+    );
+  }
+}
+
+export default Flat;
