35ab6e61f88307542b7d82344a49cfc6a22a9747
Markus Mengel <markus.mengelhx@gmail.com>
2020-06-04 14:02:29 +0200

    added flats data to flat_list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import FlatList from './flat_list.jsx';
+import flats from '../data/flats';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flats
+    };
+  }
 
   render() {
     return (
       <div className="flat-list">
-        <FlatList />
+        <FlatList flats={this.state.flats} />
       </div>
     );
   }
