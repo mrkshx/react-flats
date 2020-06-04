e9e39719ba48e74b1babc176616ddf07632ce9d2
Markus Mengel <markus.mengelhx@gmail.com>
2020-06-04 13:38:27 +0200

    imported flat_list to app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import FlatList from './flat_list.jsx';
 
 class App extends Component {
 
   render() {
     return (
-      <div />
+      <div className="flat-list">
+        <FlatList />
+      </div>
     );
   }
 }
